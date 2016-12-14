b10cddb394b241509b0fe133d6cd175c1fcd2013
kzaar <cesar_aabreu@hotmail.com>
2016-12-14 13:16:06 -0800

    Avanco na criacao da mensagem criada no webserver para ser enviada aos daemons, falta implementar crc16 e colocar na mesma, index ja especificado. Conveccao das mensagens no webserver esbocadas

--- msgredes.py
+++ msgredes.py
@@ -1,31 +1,88 @@
 #!/usr/bin/env python
 
-def mkmsg(prot, opt):
-	version = 2
+def chksum():
 	
+	return
+
+def tonbits (a , nbits) :
+	aux = "{0:b}".format(a)
+	return aux.zfill(nbits)
+		
+
+def mkmsg(prot, opt=None):
+	version = 2 #4bits
+	msg = tonbits(version, 4)
+
+	global mopt
+	mopt = ""
 	#IHL
 	#numero de WORDs 32-bit
 	#minimo = 5 (5x32 = 160bits) (sem options)
 	#maximo = 15(15x32 = 480bits)
-	IHL = 5
+	if opt == None:
+		IHL = 5 #4bits
+	else:
+		IHL = 5 + int(16*len(opt)/32) + int(16*len(opt) % 32 > 0)
+		if IHL > 15:
+			IHL = 15
+		for c in opt:
+			mopt = mopt + tonbits(ord(c), 16)			
+	msg = msg + tonbits(IHL, 4)
 
-	typeofversion = 0
-	totallenght = 20 #numero total de bytes (160bits = 20bytes)
-	idmsg = 0
-	flags = 000
-	fragoffset = 0
-	timetolive = 64
+	typeofversion = 0 #8bits
+	msg = msg + tonbits(typeofversion, 8)
+ 
+	totallenght = IHL*4 #numero total de bytes (160bits = 20bytes)
+	#16bits
+	msg = msg + tonbits(totallenght, 16)
+	
+	idmsg = 0 #16bits
+	msg = msg + tonbits(idmsg, 16)
+	
+	flags = '000'
+	msg = msg + flags
+
+	fragoffset = 0 #13bits
+	msg = msg + tonbits(fragoffset, 13)
+
+	timetolive = 64 #8bits
+	msg = msg + tonbits(timetolive, 8)
 
 	#protocol
 	#1	ps
 	#2	df
-	#3	finger
+	#3	finpger
 	#4	uptime
 	pdict = {'ps':1, 'df':2, 'finger':3, 'uptime':4}
-	protocol = pdict.get(prot)
+	protocol = pdict.get(prot) #8bits
+	msg = msg + tonbits(protocol, 8)
 
-	#headerchksum calculado por ultimo
-	srcaddr = '127.0.0.1'
-	dstaddr = '127.0.0.1'
-	options = opt ##fazer um if para tratar opt!=0 que afeta o IHL
+	#srcaddr 127.0.0.1 32bits
+	msg = msg + tonbits(127, 8)
+	msg = msg + tonbits(0 , 8)
+	msg = msg + tonbits(0 , 8)
+	msg = msg + tonbits(1 , 8)
 
+	#dstaddr 127.0.0.1 32bits
+	msg = msg + tonbits(127, 8)
+	msg = msg + tonbits(0 , 8)
+	msg = msg + tonbits(0 , 8)
+	msg = msg + tonbits(1 , 8)
+
+	if mopt:
+		msg = msg + mopt
+
+	#padding
+	pad = int((len(msg)+16) % 32)
+	if pad:
+		a = ''
+		msg = msg + a.zfill(pad)
+	
+	#crc #16bits
+	crcindex = 79
+
+	#bin to bytes
+	n = int(msg, 2)
+	data = bytes([n])	
+
+	return data
--- webserver.py
+++ webserver.py
@@ -2,8 +2,11 @@
 import cgitb
 import cgi
 import socket
+from msgredes import mkmsg
 cgitb.enable()
 form = cgi.FieldStorage()
+
+print "Content-Type: text/html\n"
 
 #	INDEX
 # 	0	ps
@@ -42,16 +45,22 @@ m3 = [form.getvalue('maq3_ps'),
 	form.getvalue('maq3_uptime'),
 	form.getvalue('maq3-uptime')]
 
-DAEMONIP = 127.0.0.1
+DAEMONIP = "127.0.0.1"
 DAEMON1 = (DAEMONIP, 9001)
 DAEMON2 = (DAEMONIP, 9002)
 DAEMON3 = (DAEMONIP, 9003)
 
 #conexao sequencial
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(DAEMON1)
+#s.connect(DAEMON1)
 
-print "Content-Type: text/html\n"
-print "M1: %s" % m1[0]
-print m1, m2, m3
+#mensagens para a maquina 1
+for i in range(len(m1))[::2]:
+	if m1[i] != None:
+		msg = mkmsg(m1[i],m1[i+1])
+		print(msg)
+		#envia a msg, aguarda a resposta e imprime
+	
+#fazer para as outra maquinas, ou colocar todas em uma matriz 
+#e fazer todas em um for
 
